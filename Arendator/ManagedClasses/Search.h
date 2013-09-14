b20647a8c083eb8748797d3ec5a926904561a36d
GrigUskov <guskov@Grigoriys-MacBook-Pro.local>
2013-09-14 10:44:57 +0400

    ManagedClasses

---
+++ Arendator/ManagedClasses/Search.h
@@ -0,0 +1,42 @@
+//
+//  Search.h
+//  Arendator
+//
+//  Created by Grig Uskov on 14/9/13.
+//  Copyright (c) 2013 Yury Nechaev. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <CoreData/CoreData.h>
+
+@class SearchResult;
+
+@interface Search : NSManagedObject
+
+@property (nonatomic, retain) NSNumber * allowedChildren;
+@property (nonatomic, retain) NSNumber * allowedPets;
+@property (nonatomic, retain) NSNumber * cityId;
+@property (nonatomic, retain) NSString * metroIdStr;
+@property (nonatomic, retain) NSNumber * optBalcony;
+@property (nonatomic, retain) NSNumber * optFridge;
+@property (nonatomic, retain) NSNumber * optFurniture;
+@property (nonatomic, retain) NSNumber * optKitchenFurniture;
+@property (nonatomic, retain) NSNumber * optPhone;
+@property (nonatomic, retain) NSNumber * optTV;
+@property (nonatomic, retain) NSNumber * optWashMachine;
+@property (nonatomic, retain) NSNumber * priceFrom;
+@property (nonatomic, retain) NSNumber * priceTo;
+@property (nonatomic, retain) NSNumber * roomFrom;
+@property (nonatomic, retain) NSNumber * roomTo;
+@property (nonatomic, retain) NSString * uid;
+@property (nonatomic, retain) NSSet *serachResults;
+@end
+
+@interface Search (CoreDataGeneratedAccessors)
+
+- (void)addSerachResultsObject:(SearchResult *)value;
+- (void)removeSerachResultsObject:(SearchResult *)value;
+- (void)addSerachResults:(NSSet *)values;
+- (void)removeSerachResults:(NSSet *)values;
+
+@end
