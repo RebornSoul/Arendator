9ef85191b90cc7526f9e2219e0d49062c862a4c9
GrigUskov <guskov@Grigoriys-MacBook-Pro.local>
2013-09-14 10:42:44 +0400

    Added data model, icons, default

---
+++ Arendator/DataModel.h
@@ -0,0 +1,40 @@
+//
+//  DataModel.h
+//  CardsOnGo
+//
+//  Created by Grig Uskov on 5/17/13.
+//  Copyright (c) 2013 ALSEDI Group. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <CoreData/CoreData.h>
+
+#define dataModel [DataModel getInstance]
+
+@interface DataModel : NSObject {
+    
+@protected
+    NSManagedObjectContext * managedObjectContext;
+    NSManagedObjectModel * managedObjectModel;
+    NSPersistentStoreCoordinator * persistentStoreCoordinator;
+}
+
++ (NSURL *)URLForDocumentsFolder;
++ (void)save;
++ (DataModel *)getInstance;
+
++ (void)deleteObject:(NSManagedObject *)object;
++ (NSManagedObject *)createObjectOfClass:(Class)objClass;
+
++ (NSArray *)allInstances:(Class)objClass;
++ (NSArray *)allInstances:(Class)objClass withPredicate:(id)stringOrPredicate,...;
+
+@end
+
+
+static NSString *generateGUID() {
+    CFUUIDRef uuidObj = CFUUIDCreate(nil);
+    NSString *uuidString = (__bridge NSString*)CFUUIDCreateString(nil, uuidObj);
+    CFRelease(uuidObj);
+    return uuidString;
+}
