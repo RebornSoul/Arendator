404b33054c412bedd09b9e152dcc92e20fcdb868
RebornSoul <nechaev.main@gmail.com>
2013-09-14 10:59:21 +0400

    added GData

---
+++ Arendator/ARCIANFetcher.m
@@ -0,0 +1,13 @@
+//
+//  ARCIANFetcher.m
+//  Arendator
+//
+//  Created by Yury Nechaev on 14.09.13.
+//  Copyright (c) 2013 Yury Nechaev. All rights reserved.
+//
+
+#import "ARCIANFetcher.h"
+
+@implementation ARCIANFetcher
+
+@end
--- Podfile
+++ Podfile
@@ -1,3 +1,4 @@
 platform :ios, '7.0'
 pod 'SBJson', '~> 3.2'
 pod 'AFNetworking', '~> 1.3.2'
+pod 'GData', '~> 1.9.1'
